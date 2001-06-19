5763e12519667946235fddb93e4d6968b9943d4d
rherveille <rherveille@572733b1-501c-411c-b915-3f8c4bc329c7>
2001-06-19 09:57:23 +0000

    revised counter.vhd

--- counter.vhd
+++ counter.vhd
@@ -6,8 +6,7 @@
 -- rev. 1.1 april 17th, 2001. Changed ro_cnt nld generation
 -- rev. 1.1 april 26th, 2001. Changed SYNCH_RCO (component ud_cnt) from string to bit. Fixed problems with Synplify
 -- rev. 1.2 may   11th, 2001. Fixed incomplete sensitivity list warning
--- rev. 1.3 june  18th, 2001. Changed "end package count" to "end count" for Xilinx tools
---
+-- rev. 1.3 june  18th, 2001. Changed module order, they are now in compilation order.
 
 
 library ieee;
@@ -15,24 +14,6 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
 package count is
-	-- run-once down-counter
-	component ro_cnt is
-	generic(SIZE : natural := 8);
-	port(
-		clk : in std_logic; -- master clock
-		nReset : in std_logic := '1'; -- asynchronous active low reset
-		rst : in std_logic := '0'; -- synchronous active high reset
-
-		cnt_en : in std_logic := '1'; -- count enable
-		go : in std_logic; -- load counter and start sequence
-		done : out std_logic; -- done counting
-		D : in unsigned(SIZE -1 downto 0); -- load counter value
-		Q : out unsigned(SIZE -1 downto 0); -- current counter value
-		
-		ID : in unsigned(SIZE -1 downto 0) := (others => '0') -- initial data after reset
-	);
-	end component ro_cnt;
-
 	-- general purpose up-down counter
 	component ud_cnt is
 	generic(
@@ -57,17 +38,8 @@ package count is
 	);
 	end component ud_cnt;
 
---end package count;
-end count;
-
---
--- run-once down-counter, counts D+1 cycles before generating 'DONE'
---
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-
-entity ro_cnt is
+	-- run-once down-counter
+	component ro_cnt is
 	generic(SIZE : natural := 8);
 	port(
 		clk : in std_logic; -- master clock
@@ -82,59 +54,8 @@ entity ro_cnt is
 		
 		ID : in unsigned(SIZE -1 downto 0) := (others => '0') -- initial data after reset
 	);
-end entity ro_cnt;
-
-architecture structural of ro_cnt is
-	component ud_cnt is
-	generic(
-		SYNCH_RCO : bit := '0'; -- NO
-		SIZE : natural := 8
-	);
-	port(
-		clk : in std_logic; -- master clock
-		nReset : in std_logic := '1'; -- asynchronous active low reset
-		rst : in std_logic := '0'; -- synchronous active high reset
-
-		cnt_en : in std_logic := '1'; -- count enable
-		ud : in std_logic := '0'; -- up / not down
-		nld : in std_logic := '1'; -- synchronous active low load
-		D : in unsigned(SIZE -1 downto 0); -- load counter value
-		Q : out unsigned(SIZE -1 downto 0); -- current counter value
-		
-		resD : in unsigned(SIZE -1 downto 0) := (others => '0'); -- initial data after reset
-
-		rci : in std_logic := '1'; -- carry input
-		rco : out std_logic -- carry output
-	);
-	end component ud_cnt;
-
-	signal rci, rco, nld : std_logic;
-begin
-	gen_ctrl: process(clk, nReset)
-	begin
-		if (nReset = '0') then
-			rci <= '0';
-		elsif (clk'event and clk = '1') then
-			if (rst = '1') then
-				rci <= '0';
-			else
-				rci <= (go or rci) and not rco;
-			end if;
-		end if;
-	end process;
-
-	nld <= not go;
-
-	-- hookup counter
-	cnt : ud_cnt 
-		generic map (SIZE => SIZE, SYNCH_RCO => '0')
-		port map (clk => clk, nReset => nReset, rst => rst, cnt_en => cnt_en, nld => nld, 
-			D => D, Q => Q, resD => ID, rci => rci, rco => rco);
-
-	done <= rco;
-end architecture structural;
-
-
+	end component ro_cnt;
+end package count;
 
 --
 -- general purpose counter
@@ -223,8 +144,76 @@ begin
 end architecture structural;
 
 
+--
+-- run-once down-counter, counts D+1 cycles before generating 'DONE'
+--
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
 
+entity ro_cnt is
+	generic(SIZE : natural := 8);
+	port(
+		clk : in std_logic; -- master clock
+		nReset : in std_logic := '1'; -- asynchronous active low reset
+		rst : in std_logic := '0'; -- synchronous active high reset
 
+		cnt_en : in std_logic := '1'; -- count enable
+		go : in std_logic; -- load counter and start sequence
+		done : out std_logic; -- done counting
+		D : in unsigned(SIZE -1 downto 0); -- load counter value
+		Q : out unsigned(SIZE -1 downto 0); -- current counter value
+		
+		ID : in unsigned(SIZE -1 downto 0) := (others => '0') -- initial data after reset
+	);
+end entity ro_cnt;
 
+architecture structural of ro_cnt is
+	component ud_cnt is
+	generic(
+		SYNCH_RCO : bit := '0'; -- NO
+		SIZE : natural := 8
+	);
+	port(
+		clk : in std_logic; -- master clock
+		nReset : in std_logic := '1'; -- asynchronous active low reset
+		rst : in std_logic := '0'; -- synchronous active high reset
 
+		cnt_en : in std_logic := '1'; -- count enable
+		ud : in std_logic := '0'; -- up / not down
+		nld : in std_logic := '1'; -- synchronous active low load
+		D : in unsigned(SIZE -1 downto 0); -- load counter value
+		Q : out unsigned(SIZE -1 downto 0); -- current counter value
+		
+		resD : in unsigned(SIZE -1 downto 0) := (others => '0'); -- initial data after reset
 
+		rci : in std_logic := '1'; -- carry input
+		rco : out std_logic -- carry output
+	);
+	end component ud_cnt;
+
+	signal rci, rco, nld : std_logic;
+begin
+	gen_ctrl: process(clk, nReset)
+	begin
+		if (nReset = '0') then
+			rci <= '0';
+		elsif (clk'event and clk = '1') then
+			if (rst = '1') then
+				rci <= '0';
+			else
+				rci <= (go or rci) and not rco;
+			end if;
+		end if;
+	end process;
+
+	nld <= not go;
+
+	-- hookup counter
+	cnt : ud_cnt 
+		generic map (SIZE => SIZE, SYNCH_RCO => '0')
+		port map (clk => clk, nReset => nReset, rst => rst, cnt_en => cnt_en, nld => nld, D => D, Q => Q, 
+			resD => ID, rci => rci, rco => rco);
+
+	done <= rco;
+end architecture structural;
